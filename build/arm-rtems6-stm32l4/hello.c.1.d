2fa4ea15052b5227d9f96ad42012e217988bf7af
Markiv9999 <vikram.srikanth.10@gmail.com>
2024-08-04 07:49:32 +0000

    commit6

--- build/arm-rtems6-stm32l4/hello.c.1.d
+++
@@ -1,2 +0,0 @@
-/home/vboxuser/projects/rtemstest1/build/arm-rtems6-stm32l4/hello.c.1.o: \
- ../../hello.c
--- build/arm-rtems6-stm32l4/init.c.1.d
+++
@@ -1,2 +0,0 @@
-/home/vboxuser/projects/rtemstest1/build/arm-rtems6-stm32l4/init.c.1.o: \
- ../../init.c
