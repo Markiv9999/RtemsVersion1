3351a6191cff1cf5b17f78bf1d8f76d406b7bce5
Markiv9999 <vikram.srikanth.10@gmail.com>
2024-08-13 06:31:47 +0000

    commit

--- build/arm-rtems6-stm32l4/hello.c.1.d
+++
@@ -1,4 +0,0 @@
-/home/vboxuser/projects/rtemstest1/build/arm-rtems6-stm32l4/hello.c.1.o: \
- ../../hello.c ../../BoardInit.h ../../stm32l4xx_hal.h \
- ../../stm32l4xx_hal_conf.h ../../stm32l4xx_hal_uart.h \
- ../../stm32l4xx_hal_uart_ex.h ../../UartConsole.h
